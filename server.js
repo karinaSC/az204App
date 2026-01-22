ce4ab02c2b6e4b0bc10dcebe930a2619e161bb22
karina campanharo <karincampanharo@gmail.com>
2026-01-22 13:04:33 -0300

    alterando logo

--- server.js
+++ server.js
@@ -1,18 +1,54 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    fs.readFile("index.html", (err, data) => {
+    // Se acessar /, abre o index.html
+    let filePath = req.url === "/" 
+        ? path.join(__dirname, "index.html") 
+        : path.join(__dirname, req.url);
+
+    // Segurança básica: evita acessar fora da pasta do projeto
+    if (!filePath.startsWith(__dirname)) {
+        res.writeHead(403);
+        res.end("Acesso negado");
+        return;
+    }
+
+    let extname = path.extname(filePath).toLowerCase();
+
+    let contentType = "text/html";
+
+    switch (extname) {
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+        case ".jpeg":
+            contentType = "image/jpeg";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".ico":
+            contentType = "image/x-icon";
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
         if (err) {
-            res.writeHead(500);
-            res.end("Erro ao carregar a página");
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Arquivo não encontrado");
             return;
         }
 
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(content);
     });
 });
 
